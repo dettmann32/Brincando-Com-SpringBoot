f322c9841eb57b724a8e8bb941478da25b9239e3
lucas <lucasdettmann8@gmail.com>
2024-12-26 20:35:29 -0300

    retTemplate post

--- configInicialEcomponentes/src/main/java/com/example/clientExemple/client.java
+++ configInicialEcomponentes/src/main/java/com/example/clientExemple/client.java
@@ -3,6 +3,7 @@ package com.example.clientExemple;
 import java.util.List;
 
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -28,5 +29,13 @@ public class client {
         //essa requisição é feita ao servidor, e retorna uma lista de objetos do tipo Anime
         ResponseEntity<List<Anime>> animePage = new RestTemplate().exchange("http://localhost:8080/animes/Pageoff", HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>(){});
         System.out.println(animePage.getBody());
+
+        //essa requisição é feita ao servidor, e salma um objeto do tipo Anime no banco de dados
+        Anime kingdom = Anime.builder().name("kingdom").build();
+        ResponseEntity<Anime> kingsomAnime = new RestTemplate().exchange("http://localhost:8080/animes", 
+        HttpMethod.POST, 
+        new HttpEntity<>(kingdom), 
+        Anime.class);
+        System.out.println(kingsomAnime.getBody());
     }
 }
