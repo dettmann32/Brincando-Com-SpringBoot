9cbabb093c92fee4b4e98e7de459f3712b339fd8
lucas <lucasdettmann8@gmail.com>
2024-12-26 20:55:15 -0300

    restTemplate put and delete

--- configInicialEcomponentes/src/main/java/com/example/clientExemple/client.java
+++ configInicialEcomponentes/src/main/java/com/example/clientExemple/client.java
@@ -2,6 +2,7 @@ package com.example.clientExemple;
 
 import java.util.List;
 
+import org.apache.coyote.Response;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -37,5 +38,16 @@ public class client {
         new HttpEntity<>(kingdom), 
         Anime.class);
         System.out.println(kingsomAnime.getBody());
+
+        //essa requisição é feita ao servidor, e atualiza um objeto do tipo Anime no banco de dados
+        kingsomAnime.getBody().setName("kingdom 2");
+        ResponseEntity<Void> kingdomUpdate = new RestTemplate().exchange("http://localhost:8080/animes", 
+        HttpMethod.POST, kingsomAnime, Void.class);
+        System.out.println(kingdomUpdate.getHeaders());
+
+        //essa requisição é feita ao servidor, e deleta um objeto do tipo Anime no banco de dados
+        ResponseEntity<Void> kingdomDelete = new RestTemplate().exchange("http://localhost:8080/animes/{id}", 
+        HttpMethod.DELETE, kingsomAnime, Void.class, kingsomAnime.getBody().getId());
+        System.out.println(kingdomDelete.getHeaders());
     }
 }
